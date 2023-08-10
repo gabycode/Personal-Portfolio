86ba7539d78558309462c8f2eb48a2e9442d2418
gabycode <86427446+gabycode@users.noreply.github.com>
2023-08-10 15:55:32 -0400

    add: sidebar component

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+import Sidebar from '../Sidebar'
+import './index.scss'
+
+const Layout = () => {
+  return (
+    <>
+      <Sidebar />
+    </>
+  )
+}
+
+export default Layout
