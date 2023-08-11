1c57c39d266aef21c49863ac9a8ba735f75f12ae
gabycode <86427446+gabycode@users.noreply.github.com>
2023-08-11 12:35:53 -0400

    add: home route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.scss'
 import { Route, Routes } from 'react-router'
 import Layout from './components/Layout'
+import Home from './components/Home'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +10,9 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </>
   )
