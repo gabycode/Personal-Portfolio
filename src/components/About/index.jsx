fc9c854407047032b420c7f054a3f58da9c6babb
gabycode <86427446+gabycode@users.noreply.github.com>
2023-08-17 11:11:11 -0400

    add: loader

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -3,13 +3,14 @@ import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
-  faCss3,
+  faCss3Alt,
   faFigma,
   faGitAlt,
   faHtml5,
   faJsSquare,
   faReact,
 } from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
 
 const About = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -20,54 +21,57 @@ const About = () => {
     }, 3000)
   }, [])
   return (
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
-            idx={15}
-          />
-        </h1>
-        <p>
-          Hello! 👋 I'm a Frontend Developer who loves turning design dreams
-          into interactive web wonders. You might consider me quiet but I'm
-          silently confident and naturally curious.
-        </p>
-        <p>
-          My passion lies in creating user-friendly interfaces that not only
-          look stunning but also deliver seamless interactions. When I'm not
-          coding, you will find me enjoying videogames or venturing into the
-          great outdoors.
-        </p>
-        <p>
-          Let's team up and build some awesome digital experiences together!
-        </p>
-      </div>
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            Hello! 👋 I'm a Frontend Developer who loves turning design dreams
+            into interactive web wonders. You might consider me quiet but I'm
+            silently confident and naturally curious.
+          </p>
+          <p>
+            My passion lies in creating user-friendly interfaces that not only
+            look stunning but also deliver seamless interactions. When I'm not
+            coding, you will find me enjoying videogames or venturing into the
+            great outdoors.
+          </p>
+          <p>
+            Let's team up and build some awesome digital experiences together!
+          </p>
+        </div>
 
-      <div className="stage-cube-cont">
-        <div className="cubespinner">
-          <div className="face1">
-            <FontAwesomeIcon icon={faFigma} color="#878483" />
-          </div>
-          <div className="face2">
-            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-          </div>
-          <div className="face3">
-            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
-          </div>
-          <div className="face4">
-            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
-          </div>
-          <div className="face5">
-            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
-          </div>
-          <div className="face6">
-            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faFigma} color="#000" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3Alt} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <Loader type="ball-spin-fade-loader" />
+    </>
   )
 }
 
