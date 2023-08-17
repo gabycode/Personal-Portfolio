e1e05f8b40ab0fe119d8ab0fa8dd719714a39680
gabycode <86427446+gabycode@users.noreply.github.com>
2023-08-16 23:26:37 -0400

    add: about page content

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,5 +1,70 @@
 import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
 
-const About = () => 'hello'
+const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+  return (
+    <div className="container about-page">
+      <div className="text-zone">
+        <h1>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+            idx={15}
+          />
+        </h1>
+        <p>
+          Hello! 👋 I'm a Frontend Developer who loves turning design dreams
+          into interactive web wonders. You might consider me quiet but I'm
+          silently confident and naturally curious.
+        </p>
+        <p>
+          My passion lies in creating user-friendly interfaces that not only
+          look stunning but also deliver seamless interactions. When I'm not
+          coding, you will find me enjoying videogames or venturing into the
+          great outdoors.
+        </p>
+        <p>
+          Let's team up and build some awesome digital experiences together!
+        </p>
+      </div>
+
+      <div className="stage-cube-cont">
+        <div className="cubespinner">
+          <div className="face1">
+            <FontAwesomeIcon icon={faReact} color="#dd0031" />
+          </div>
+          <div className="face2">
+            <FontAwesomeIcon icon={faHtml5} color="#dd0031" />
+          </div>
+          <div className="face3">
+            <FontAwesomeIcon icon={faCss3} color="#dd0031" />
+          </div>
+          <div className="face4">
+            <FontAwesomeIcon icon={faJsSquare} color="#dd0031" />
+          </div>
+          <div className="face5">
+            <FontAwesomeIcon icon={faGitAlt} color="#dd0031" />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default About
