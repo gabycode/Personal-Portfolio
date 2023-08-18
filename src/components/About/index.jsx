ae402a6a46bc75bad63562e9d4494a6a33b97fad
gabycode <86427446+gabycode@users.noreply.github.com>
2023-08-18 00:06:39 -0400

    add: icons spinnig

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faCss3,
+  faFigma,
   faGitAlt,
   faHtml5,
   faJsSquare,
@@ -47,19 +48,22 @@ const About = () => {
       <div className="stage-cube-cont">
         <div className="cubespinner">
           <div className="face1">
-            <FontAwesomeIcon icon={faReact} color="#dd0031" />
+            <FontAwesomeIcon icon={faFigma} color="#878483" />
           </div>
           <div className="face2">
-            <FontAwesomeIcon icon={faHtml5} color="#dd0031" />
+            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
           </div>
           <div className="face3">
-            <FontAwesomeIcon icon={faCss3} color="#dd0031" />
+            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
           </div>
           <div className="face4">
-            <FontAwesomeIcon icon={faJsSquare} color="#dd0031" />
+            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
           </div>
           <div className="face5">
-            <FontAwesomeIcon icon={faGitAlt} color="#dd0031" />
+            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+          </div>
+          <div className="face6">
+            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
           </div>
         </div>
       </div>
